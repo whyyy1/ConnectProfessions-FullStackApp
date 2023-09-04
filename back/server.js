6f9739aed21ec753c33e7ba908519e094e2d8422
whyyy1 <dysarttieran@gmail.com>
2023-09-04 14:39:13 -0600

    got rid of the fileUpload

--- back/server.js
+++ back/server.js
@@ -2,24 +2,24 @@ const express = require('express')
 require('dotenv')
 const cors = require('cors')
 const methodOverride = require('method-override')
-const fileUpload = require('express-fileupload');
+
 const {mongoConfig}  = require('./config')
 const newsData = require('./data/newsData')
 mongoConfig()
 const multer = require('multer');
 
-// Set up multer with a destination folder and file name customization
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'uploads/'); // Set the destination folder where files will be saved
-  },
-  filename: (req, file, callback) => {
-    const fileName = Date.now() + '-' + file.originalname; // Customize the file name
-    callback(null, fileName);
-  },
-});
+// // Set up multer with a destination folder and file name customization
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, 'uploads/'); // Set the destination folder where files will be saved
+//   },
+//   filename: (req, file, callback) => {
+//     const fileName = Date.now() + '-' + file.originalname; // Customize the file name
+//     callback(null, fileName);
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
 
 const app = express()
@@ -29,12 +29,12 @@ app.use(cors())
 app.use(methodOverride('_method'))
 app.use(express.json())
 app.use(express.static('public'))
-app.use('/cp',fileUpload({
-    useTempFiles: true,
-    safeFileNames: true,
-    preserveExtension: true,
-    tempFileDir: `${__dirname}/public/files/temp`
-  }), cp)
+// app.use('/cp',fileUpload({
+//     useTempFiles: true,
+//     safeFileNames: true,
+//     preserveExtension: true,
+//     tempFileDir: `${__dirname}/public/files/temp`
+//   }), cp)
 
 app.get('/',(req,res) => {
     // console.log('hey',newsData)
